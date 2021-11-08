3190c445ce4cb24adb3f215a4655332973b4d5a2
Kenji Asaba <kasa@akiya-souken.co.jp>
2021-11-08 10:49:38 +0900

    opset12

--- createOnnx.py
+++ createOnnx.py
@@ -11,12 +11,13 @@ batch_size = 1
 height = 64
 width = 64
 x = torch.randn((batch_size, height, width, 3), requires_grad=True).byte().to(gpu)
+opset = 12
 ##############################################################################
 
 input_names = ["input"] #[B,64,64,3],
 output_names = ['eye', 'iris'] #[B,71,3], [B,5,3]
 
-onnx_file_name = "BlazeIris_{}_{}_{}_BGRxByte.onnx".format(batch_size, height, width)
+onnx_file_name = "BlazeIris_{}x{}x{}xBGRxByte_opset{}.onnx".format(batch_size, height, width, opset)
 dynamic_axes = {
     "input": {0: "batch_size"}, 
     "eye": {0: "batch_size"}, 
@@ -27,9 +28,9 @@ torch.onnx.export(net,
                 x,
                 onnx_file_name,
                 export_params=True,
-                opset_version=11,
+                opset_version=opset,
                 do_constant_folding=True,
                 input_names=input_names, 
                 output_names=output_names
-                ,dynamic_axes=dynamic_axes
+                #,dynamic_axes=dynamic_axes
                 )
